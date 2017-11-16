cbe3173baa7b7d4407d686fc9e8e90de3ef656fa
Harshal <harshal.carpenter@gmail.com>
2017-11-16 00:13:17 -0800

    initial setup

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp');
+var webpack = require('gulp-webpack');
+var runSequence = require('run-sequence');
+
+//Running Webpack and Copy
+gulp.task('default', function() {
+    runSequence('webpack','copy', function(){
+        console.log("Done!");
+    })
+});
+
+//Copy files to WWW Folder
+gulp.task('copy', function(){
+   gulp.src(['style/**/*.css'])
+       .pipe(gulp.dest('www/style'))
+
+    gulp.src(['dist/**'])
+       .pipe(gulp.dest('www/dist'))
+
+    gulp.src(['*.html'])
+       .pipe(gulp.dest('www'))
+});
+
+//Running Webpack
+gulp.task('webpack', function () {
+    return gulp.src('src/index.js')
+        .pipe(webpack( require('./webpack.config.js') ))
+        .pipe(gulp.dest('dist/'))
+})
